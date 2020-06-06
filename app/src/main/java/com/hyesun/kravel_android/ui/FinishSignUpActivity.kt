8ee13cf9f7bb90f2332ba635d6a3d5340f0398d9
yunakim2 <98_02@naver.com>
2020-06-06 15:12:41 +0900

    [yuna] fragment 생성 + signupView signupfinishView 수정

--- app/src/main/java/com/hyesun/kravel_android/ui/FinishSignUpActivity.kt
+++ app/src/main/java/com/hyesun/kravel_android/ui/FinishSignUpActivity.kt
@@ -1,13 +1,21 @@
 package com.hyesun.kravel_android.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hyesun.kravel_android.R
+import kotlinx.android.synthetic.main.activity_finish_sign_up.*
 
 class FinishSignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish_sign_up)
+
+
+        btn_signup_finish.setOnClickListener {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
