095a46fc426046c5cbf728437a1241995df60577
yanghyesun <nbn0809@naver.com>
2020-06-12 19:37:48 +0900

    [hyesun] setting language view

---
+++ app/src/main/java/com/hyesun/kravel_android/ui/SetLanguageActivity.kt
@@ -0,0 +1,11 @@
+package com.hyesun.kravel_android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetLanguageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_language)
+    }
+}
