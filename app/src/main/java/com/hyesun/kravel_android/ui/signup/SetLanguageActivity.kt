f87495f90e61eaf6ba96858442489b18b893bb16
yanghyesun <nbn0809@naver.com>
2020-07-24 21:10:31 +0900

    [hyesun] UPDATE: 로그인 뷰 수정

--- app/src/main/java/com/hyesun/kravel_android/ui/signup/SetLanguageActivity.kt
+++ app/src/main/java/com/hyesun/kravel_android/ui/signup/SetLanguageActivity.kt
@@ -2,6 +2,7 @@ package com.hyesun.kravel_android.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hyesun.kravel_android.R
 
 class SetLanguageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
