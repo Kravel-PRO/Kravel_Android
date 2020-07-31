49e21e6c3645058fa2a7b5bddb345cfdcf4ecad9
yunakim2 <98_02@naver.com>
2020-07-31 23:32:48 +0900

    [yuna] UPDATE : tmap 현재위치

--- app/src/main/java/com/hyesun/kravel_android/ui/map/MapFragment.kt
+++ app/src/main/java/com/hyesun/kravel_android/ui/map/MapFragment.kt
@@ -1,5 +1,7 @@
 package com.hyesun.kravel_android.ui.map
 
+import android.app.Activity
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,15 +9,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.hyesun.kravel_android.R
+import com.skt.Tmap.TMapGpsManager
 import com.skt.Tmap.TMapView
+import kotlinx.android.synthetic.main.fragment_map.*
 import org.koin.core.context.GlobalContext
 import timber.log.Timber
 
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), TMapGpsManager.onLocationChangedCallback {
 
-    val apiKey = "l7xx8006432e92764b7bb7edecc3a9a5543c"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,6 +31,10 @@ class MapFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         initMap()
+
+        togglebtn_gps.setOnClickListener {
+            initGPS()
+        }
     }
     private fun initMap() {
         val tmap = view?.findViewById<LinearLayout>(R.id.map)
@@ -40,6 +48,22 @@ class MapFragment : Fragment() {
         tmapView.setTrackingMode(true)
         tmapView.setSightVisible(true)
         tmap?.addView(tmapView)
+
+    }
+    private fun initGPS() {
+        val tmapgps : TMapGpsManager = TMapGpsManager(context)
+        tmapgps.provider= TMapGpsManager.GPS_PROVIDER
+        tmapgps.minTime = 1000
+        tmapgps.OpenGps()
+        Timber.d("${tmapgps.location}")
+
+
+
+    }
+
+    override fun onLocationChange(p0: Location?) {
+        val lat : Double = p0!!.latitude
+        val lon : Double = p0!!.longitude
 
     }
 
