a312d3d42daa13060977c3c03be1fe7e9ca1c437
yunakim2 <98_02@naver.com>
2020-08-06 18:53:04 +0900

    [yuna] UPDATE : Mapview - near place Recyclerview

---
+++ app/src/main/java/com/hyesun/kravel_android/ui/adapter/MapPlaceRecyclerview.kt
@@ -0,0 +1,50 @@
+package com.hyesun.kravel_android.ui.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.hyesun.kravel_android.R
+import com.hyesun.kravel_android.common.GlideApp
+import com.hyesun.kravel_android.data.mock.NearPlaceData
+import com.hyesun.kravel_android.data.response.DetailPlaceResponse
+import com.hyesun.kravel_android.util.dpToPx
+import com.hyesun.kravel_android.util.inflate
+import com.hyesun.kravel_android.util.setRound
+
+
+class MapPlaceRecyclerview() : RecyclerView.Adapter<MapPlaceRecyclerview.ViewHolder>() {
+
+    private var data: List<NearPlaceData> = emptyList()
+
+    fun initData(data: List<NearPlaceData>){
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
+            = ViewHolder(parent.inflate(R.layout.item_map_near_place))
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+        holder.setIsRecyclable(false)
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val img : ImageView = itemView.findViewById(R.id.img_map_near)
+        private val txtPlace : TextView = itemView.findViewById(R.id.txt_map_near_title)
+        private val txtAddress : TextView = itemView.findViewById(R.id.txt_map_near_address)
+        private val clContent : ConstraintLayout = itemView.findViewById(R.id.cl_map_near)
+
+        fun bind(item: NearPlaceData){
+            GlideApp.with(itemView).load(item.img).into(img)
+            clContent.setRound(5.dpToPx().toFloat())
+            txtPlace.text = item.placeTitle
+            txtAddress.text = item.address
+        }
+    }
+}
--- app/src/main/java/com/hyesun/kravel_android/ui/map/MapFragment.kt
+++ app/src/main/java/com/hyesun/kravel_android/ui/map/MapFragment.kt
@@ -15,8 +15,14 @@ import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.hyesun.kravel_android.R
+import com.hyesun.kravel_android.common.HorizontalItemDecorator
+import com.hyesun.kravel_android.data.mock.NearPlaceData
+import com.hyesun.kravel_android.data.response.DetailPlaceResponse
+import com.hyesun.kravel_android.ui.adapter.MapPlaceRecyclerview
+import com.hyesun.kravel_android.ui.adapter.PopularRecyclerview
 import com.skt.Tmap.TMapGpsManager
 import com.skt.Tmap.TMapView
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_map.*
 import org.koin.core.context.GlobalContext
 import timber.log.Timber
@@ -25,6 +31,7 @@ import java.util.*
 
 
 class MapFragment : Fragment(), TMapGpsManager.onLocationChangedCallback {
+    private val nearAdapter : MapPlaceRecyclerview by lazy { MapPlaceRecyclerview() }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,10 +43,28 @@ class MapFragment : Fragment(), TMapGpsManager.onLocationChangedCallback {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         initMap()
+        initRecycler()
 
         togglebtn_gps.setOnClickListener {
 
         }
+    }
+    private fun initRecycler() {
+        rv_map_near_place.apply {
+            adapter = nearAdapter
+            addItemDecoration(HorizontalItemDecorator(16))
+        }
+
+        nearAdapter.initData(
+            listOf(
+                NearPlaceData("호텔델루나", arrayListOf("#아이유","#여진구"),"https://www.dramamilk.com/wp-content/uploads/2019/07/Hotel-de-Luna-episode-5-live-recap-IU.jpg","호텔델루나 주소"),
+                NearPlaceData("호텔델루나", arrayListOf("#아이유","#여진구"),"https://www.dramamilk.com/wp-content/uploads/2019/07/Hotel-de-Luna-episode-5-live-recap-IU.jpg","호텔델루나 주소"),
+                NearPlaceData("호텔델루나", arrayListOf("#아이유","#여진구"),"https://www.dramamilk.com/wp-content/uploads/2019/07/Hotel-de-Luna-episode-5-live-recap-IU.jpg","호텔델루나 주소"),
+                NearPlaceData("호텔델루나", arrayListOf("#아이유","#여진구"),"https://www.dramamilk.com/wp-content/uploads/2019/07/Hotel-de-Luna-episode-5-live-recap-IU.jpg","호텔델루나 주소"),
+                NearPlaceData("호텔델루나", arrayListOf("#아이유","#여진구"),"https://www.dramamilk.com/wp-content/uploads/2019/07/Hotel-de-Luna-episode-5-live-recap-IU.jpg","호텔델루나 주소")
+
+            )
+        )
     }
     private fun initMap() {
         val tmap = view?.findViewById<LinearLayout>(R.id.map)
