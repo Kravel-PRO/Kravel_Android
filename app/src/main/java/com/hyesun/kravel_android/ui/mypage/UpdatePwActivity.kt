8b0e2e5d15596775eead77731bc9fc9633ff86ea
yanghyesun <nbn0809@naver.com>
2020-08-07 18:59:34 +0900

    [hyesun] UPDATE : 비밀번호 수정 뷰 구현

---
+++ app/src/main/java/com/hyesun/kravel_android/ui/mypage/UpdatePwActivity.kt
@@ -0,0 +1,11 @@
+package com.hyesun.kravel_android.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdatePwActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_pw)
+    }
+}
