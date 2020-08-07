d8dabc32671f8ee7bf000f7f9c903091bd6cbdb9
yanghyesun <nbn0809@naver.com>
2020-08-07 19:13:47 +0900

    [hyesun] UPDATE : 언어 설정 뷰 수정

--- app/src/main/java/com/hyesun/kravel_android/ui/mypage/UpdatePwActivity.kt
+++ app/src/main/java/com/hyesun/kravel_android/ui/mypage/UpdatePwActivity.kt
@@ -2,6 +2,7 @@ package com.hyesun.kravel_android.ui.mypage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hyesun.kravel_android.R
 
 class UpdatePwActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
