9495a9a256213925f45981f56e340ae574647333
yanghyesun <nbn0809@naver.com>
2020-08-14 23:51:23 +0900

    [hyesun] UPDATE : 이미지 파일 수정

--- app/src/main/java/com/hyesun/kravel_android/ui/mypage/UpdatePwActivity.kt
+++ app/src/main/java/com/hyesun/kravel_android/ui/mypage/UpdatePwActivity.kt
@@ -3,10 +3,23 @@ package com.hyesun.kravel_android.ui.mypage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hyesun.kravel_android.R
+import com.hyesun.kravel_android.util.onTextChangeListener
+import kotlinx.android.synthetic.main.activity_update_pw.*
 
 class UpdatePwActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_pw)
+
+        initChangeText()
+    }
+
+    private fun initChangeText(){
+        edt_update_pw_content.onTextChangeListener(
+            onTextChanged = {
+                if(!edt_update_pw_content.text.isNullOrBlank()) edt_update_pw_content.setBackgroundResource(R.drawable.signup_edit_style_true)
+                else edt_update_pw_content.setBackgroundResource(R.drawable.signup_edit_style)
+            }
+        )
     }
 }
