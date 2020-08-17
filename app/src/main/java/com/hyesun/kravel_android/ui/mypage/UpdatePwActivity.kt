dc554d1fbf8e1c29a2f4ab9959983a4e4a48a697
yanghyesun <nbn0809@naver.com>
2020-08-18 00:27:54 +0900

    [hyesun] UPDATE : 내 비밀번호 수정 뷰 수정

--- app/src/main/java/com/hyesun/kravel_android/ui/mypage/UpdatePwActivity.kt
+++ app/src/main/java/com/hyesun/kravel_android/ui/mypage/UpdatePwActivity.kt
@@ -1,9 +1,14 @@
 package com.hyesun.kravel_android.ui.mypage
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hyesun.kravel_android.R
+import com.hyesun.kravel_android.common.setOnDebounceClickListener
 import com.hyesun.kravel_android.util.onTextChangeListener
+import com.hyesun.kravel_android.util.setInvisible
+import com.hyesun.kravel_android.util.setVisible
+import com.hyesun.kravel_android.util.startActivity
 import kotlinx.android.synthetic.main.activity_update_pw.*
 
 class UpdatePwActivity : AppCompatActivity() {
@@ -11,15 +16,50 @@ class UpdatePwActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_pw)
 
-        initChangeText()
+        initCheckPersonal()
+        initChangeEditText()
+        initEnableBtn()
     }
 
-    private fun initChangeText(){
+    private fun initCheckPersonal(){
+        btn_update_pw_check.setOnDebounceClickListener {
+            cl_update_pw_content.setInvisible()
+            cl_update_pw_content2.setVisible()
+        }
+    }
+
+    private fun initChangeEditText(){
         edt_update_pw_content.onTextChangeListener(
             onTextChanged = {
                 if(!edt_update_pw_content.text.isNullOrBlank()) edt_update_pw_content.setBackgroundResource(R.drawable.signup_edit_style_true)
                 else edt_update_pw_content.setBackgroundResource(R.drawable.signup_edit_style)
             }
         )
+
+        edt_update_pw_change.onTextChangeListener(
+            onTextChanged = {
+                if(!edt_update_pw_change.text.isNullOrBlank()) edt_update_pw_change.setBackgroundResource(R.drawable.signup_edit_style_true)
+                else edt_update_pw_change.setBackgroundResource(R.drawable.signup_edit_style)
+                initEnableBtn()
+            }
+        )
+
+        edt_update_pw_change_check.onTextChangeListener(
+            onTextChanged = {
+                if(!edt_update_pw_change_check.text.isNullOrBlank()) edt_update_pw_change_check.setBackgroundResource(R.drawable.signup_edit_style_true)
+                else edt_update_pw_change_check.setBackgroundResource(R.drawable.signup_edit_style)
+                initEnableBtn()
+            }
+        )
+    }
+
+    private fun initEnableBtn(){
+        if(!edt_update_pw_change.text.isNullOrBlank() and !edt_update_pw_change_check.text.isNullOrBlank()){
+            btn_update_pw_complete.isEnabled = true
+            btn_update_pw_complete.setTextColor(Color.WHITE)
+        } else {
+            btn_update_pw_complete.isEnabled = false
+            btn_update_pw_complete.setTextColor(resources.getColor(R.color.colorDarkGrey))
+        }
     }
 }
