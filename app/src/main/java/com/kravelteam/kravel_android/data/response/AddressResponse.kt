224e09da2d0f494a277fc7c99d4f99fd9ba352bc
yanghyesun <nbn0809@naver.com>
2020-09-03 00:04:38 +0900

    [hyesun] UPDATE : 주소 검색 api 구현중

---
+++ app/src/main/java/com/kravelteam/kravel_android/data/response/AddressResponse.kt
@@ -0,0 +1,7 @@
+package com.kravelteam.kravel_android.data.response
+
+data class AddressResponse(
+    val place_name: String,
+    val address_name: String,
+    val road_address_name: String
+)
---
+++ app/src/main/java/com/kravelteam/kravel_android/ui/mypage/AddressActivity.kt
@@ -0,0 +1,12 @@
+package com.kravelteam.kravel_android.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kravelteam.kravel_android.R
+
+class AddressActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_address)
+    }
+}
