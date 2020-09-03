0868baacb13f0577c0724ec58041239a915eb697
yanghyesun <nbn0809@naver.com>
2020-09-03 16:50:22 +0900

    [hyesun] UPDATE : 카메라 SdCard 저장 완료

--- app/src/main/java/com/kravelteam/kravel_android/ui/map/CameraActivity.kt
+++ app/src/main/java/com/kravelteam/kravel_android/ui/map/CameraActivity.kt
@@ -1,28 +1,31 @@
 package com.kravelteam.kravel_android.ui.map
 
 import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.Camera
+import android.media.MediaScannerConnection
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.os.Environment
+import android.provider.MediaStore
+import android.webkit.MimeTypeMap
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.LifecycleOwner
+import androidx.core.net.toFile
 import com.google.common.util.concurrent.ListenableFuture
 import com.kravelteam.kravel_android.R
 import kotlinx.android.synthetic.main.activity_camera.*
 import timber.log.Timber
 import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import java.util.jar.Manifest
 
 
 class CameraActivity : AppCompatActivity(){
@@ -57,18 +60,46 @@ class CameraActivity : AppCompatActivity(){
 
     }
 
+
     private fun takePhoto() {
         // Get a stable reference of the modifiable image capture use case
         val imageCapture = imageCapture ?: return
 
-        // Create time-stamped output file to hold the image
-        val photoFile = File(
-            outputDirectory,
-            SimpleDateFormat(FILENAME_FORMAT, Locale.US
-            ).format(System.currentTimeMillis()) + ".jpg")
+        val values = ContentValues()
+
+        val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString() +
+                File.separator +
+                "Camera"
+        val file = File(dir)
+        if (!file.exists()) {
+            file.mkdirs()
+        }
+
+        val f = File(file, "test_capture.jpg")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            with(values) {
+                put(MediaStore.Images.Media.TITLE, "Kravel")
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/Camera")
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            }
+        } else {
+            with(values) {
+                put(MediaStore.Images.Media.TITLE, "Kravel")
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.BUCKET_ID, "Kravel")
+                put(MediaStore.Images.Media.DATA, f.absolutePath)
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            }
+        }
 
         // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            applicationContext.contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            values
+        ).build()
 
         // Set up image capture listener, which is triggered after photo has
         // been taken
@@ -79,10 +110,35 @@ class CameraActivity : AppCompatActivity(){
                 }
 
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val savedUri = Uri.fromFile(photoFile)
-                    val msg = "Photo capture succeeded: $savedUri"
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Timber.d(msg)
+                    val savedUri = Uri.fromFile(f)
+
+                    // We can only change the foreground Drawable using API level 23+ API
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        // Update the gallery thumbnail with latest picture taken
+                        //setGalleryThumbnail(savedUri)
+                    }
+
+                    // Implicit broadcasts will be ignored for devices running API level >= 24
+                    // so if you only target API level 24+ you can remove this statement
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                        this@CameraActivity.sendBroadcast(
+                            Intent(Camera.ACTION_NEW_PICTURE, savedUri)
+                        )
+                    }
+
+                    // If the folder selected is an external media directory, this is
+                    // unnecessary but otherwise other apps will not be able to access our
+                    // images unless we scan them using [MediaScannerConnection]
+                    val mimeType = MimeTypeMap.getSingleton()
+                        .getMimeTypeFromExtension(savedUri?.toFile()?.extension)
+                    Timber.e(savedUri?.toFile()?.absolutePath)
+                    MediaScannerConnection.scanFile(
+                        applicationContext,
+                        arrayOf(savedUri?.toFile()?.absolutePath),
+                        arrayOf(mimeType)
+                    ) { _, uri ->
+                        Timber.e( "Image capture scanned into media store: $uri")
+                    }
                 }
             })
     }
@@ -94,7 +150,9 @@ class CameraActivity : AppCompatActivity(){
 
     private fun getOutputDirectory(): File {
         val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+            Timber.e("${it.absolutePath}")
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
         return if (mediaDir != null && mediaDir.exists())
             mediaDir else filesDir
     }
@@ -113,6 +171,9 @@ class CameraActivity : AppCompatActivity(){
                     it.setSurfaceProvider(preview_camera.createSurfaceProvider())
                 }
 
+            imageCapture = ImageCapture.Builder()
+                .build()
+
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
@@ -122,11 +183,9 @@ class CameraActivity : AppCompatActivity(){
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
+                    this, cameraSelector, preview, imageCapture)
 
-            } catch(exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
+            } catch(exc: Exception) {}
 
         }, ContextCompat.getMainExecutor(this))
     }
